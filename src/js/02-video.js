d20053402025545d398cf228e0c23818dd3df5e3
sergey alekseev <alekseev_ss@ukr.net>
2022-12-19 21:46:07 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,7 +6,7 @@ const player = new Player(iframe);
 const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 player.on('timeupdate', throttle(onPlay, 1000));
 function onPlay({ seconds }) {
-  localStorage.setItem('LOCALSTORAGE_KEY', seconds);
+  localStorage.setItem(LOCALSTORAGE_KEY, seconds);
 }
 
-player.setCurrentTime(localStorage.getItem('LOCALSTORAGE_KEY'));
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
